715cf516c9bc619253cf12f0d8f336a2cc966e95
castillo05 <jorgeantonio20142014@gmail.com>
2020-03-08 21:25:31 -0600

    Add card and grid for blog list

--- src/views/Components/Components.js
+++ src/views/Components/Components.js
@@ -28,15 +28,46 @@ import SectionLogin from "./Sections/SectionLogin.js";
 import SectionExamples from "./Sections/SectionExamples.js";
 import SectionDownload from "./Sections/SectionDownload.js";
 
+// Grid
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+// Card Action
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+
 import styles from "assets/jss/material-kit-react/views/components.js";
 
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles(theme=> ({
+  root: {
+    flexGrow: 1,
+    maxWidth:400,
+  },
+  media:{
+    height:140
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const data=[0,1,2,3];
+
+const useStyles2=makeStyles(styles);
 
 export default function Components(props) {
-  const classes = useStyles();
+  const classes = useStyles2();
+  const classes2=useStyles();
   const { ...rest } = props;
   return (
     <div>
+      
       <Header
         brand="JC Developer"
         rightLinks={<HeaderLinks />}
@@ -62,9 +93,47 @@ export default function Components(props) {
           </GridContainer>
         </div>
       </Parallax>
-
+     
       <div className={classNames(classes.main, classes.mainRaised)}>
-        <SectionBasics />
+      <div className={classes.root}>
+      <Grid container spacing={3}>
+        {data.map(d => (
+          <Grid item xs={12} sm={6} lg={3}>
+                    {/* Card */}
+                            <Card className={classes2.root}>
+                              <CardActionArea>
+                                <CardMedia
+                                  className={classes2.media}
+                                  image=""
+                                  title="Contemplative Reptile"
+                                />
+                                <CardContent>
+                                  <Typography gutterBottom variant="h5" component="h2">
+                                    Lizard
+                                  </Typography>
+                                  <Typography variant="body2" color="textSecondary" component="p">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                                    across all continents except Antarctica
+                                  </Typography>
+                                </CardContent>
+                              </CardActionArea>
+                              <CardActions>
+                                <Button size="small" color="primary">
+                                  Share
+                                </Button>
+                                <Button size="small" color="primary">
+                                  Learn More
+                                </Button>
+                              </CardActions>
+                      </Card>
+                      {/* End Card */}
+                  </Grid>
+        ))}
+        
+       
+      </Grid>
+    </div>
+        {/* <SectionBasics />
         <SectionNavbars />
         <SectionTabs />
         <SectionPills />
@@ -82,7 +151,7 @@ export default function Components(props) {
           </Link>
         </GridItem>
         <SectionExamples />
-        <SectionDownload />
+        <SectionDownload /> */}
       </div>
       <Footer />
     </div>
